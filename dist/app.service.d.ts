75745f4e483f5572659f63a89ee7a9aed6d8bf56
Pablo Winter <contato@pablowinter.com.br>
2025-07-16 16:55:22 -0300

    delete dist

--- dist/app.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { CustomLoggerService } from './tracing/custom-logger.service';
-export declare class AppService {
-    private readonly logger;
-    constructor(logger: CustomLoggerService);
-    getHello(): string;
-    getHealthCheck(): object;
-}
--- dist/tracing/tracing.middleware.d.ts
+++
@@ -1,8 +0,0 @@
-import { NestMiddleware } from '@nestjs/common';
-import { Request, Response, NextFunction } from 'express';
-import { TracingContextService } from './tracing-context.service';
-export declare class TracingMiddleware implements NestMiddleware {
-    private readonly tracingContextService;
-    constructor(tracingContextService: TracingContextService);
-    use(req: Request, res: Response, next: NextFunction): void;
-}
